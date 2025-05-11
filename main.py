d7d0e2986b9b2ac475335461c8e05caaedf600d5
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-05-12 05:15:19 +0800

    Intialise Project

---
+++ main.py
@@ -0,0 +1,10 @@
+def function():
+    print("Created File")
+
+function()
+
+# Import Packages
+# Import Audio Files / Ask User for Audio File
+# Loading Empire Arrays
+# Plotting the Magnitude Spectrum
+# Display Graphical Overview
