a781be8e29eda2cd92f2aa7fc47a98707e806121
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-05-12 06:29:38 +0800

    Implement Proper Conversion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Import Packages
+# ----- Import Packages -----
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -8,15 +8,26 @@ from pydub import AudioSegment
 from pydub.playback import play
 # Note: Import ffmpeg
 
-# Import Audio Files / Ask User for Audio File
+# ----- Import Audio Files / Ask User for Audio File -----
 BASE_DIR = './audio'
-audio_file = 'all_i_ask_palmer.mp3'
+audio_file_path = 'all_i_ask_palmer' # Change according to your audio
+original_format = '.mp3'
+converted_format = '.wav'
 
-# Convert MP3 files to WAV files
-converted_file = AudioSegment.from_mp3(os.path.join(BASE_DIR, audio_file))
-# ipd.Audio(converted_file) # This is for jupyter notebook
-play(converted_file)
+# ----- Convert MP3 files to WAV files -----
+EXPORT_DIR = './converted_audio'
+converted_file = AudioSegment.from_mp3(os.path.join(BASE_DIR, audio_file_path + original_format))
+converted_file_path = os.path.join(EXPORT_DIR, os.path.join(audio_file_path + converted_format))
+converted_file.export(converted_file_path, format='wav')
+# ipd.Audio(converted_file) # Play audio on Jupyter Notebook
+# play(converted_file) # Play audio on PyCharm
 
-# Loading Empire Arrays
-# Plotting the Magnitude Spectrum
-# Display Graphical Overview
+# ----- Loading Empire Arrays -----
+audio_c4, sr = librosa.load(converted_file_path)
+print(audio_c4.shape) # c4 is the signal
+# Extract Fourier Transform Coefficients to have the #bins = #samples
+audio_ft = np.fft.fft(audio_c4)
+print(audio_ft.shape)
+
+# ----- Plotting the Magnitude Spectrum -----
+# ----- Display Graphical Overview -----
