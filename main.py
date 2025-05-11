e70a27295457f2168f71cf1eada47c30d9758bab
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-05-12 05:56:00 +0800

    Import Packages

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-def function():
-    print("Created File")
-
-function()
-
 # Import Packages
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import librosa
+import IPython.display as ipd
+
 # Import Audio Files / Ask User for Audio File
+BASE_DIR = './audio'
+
+# Convert MP3 files to WAV files
 # Loading Empire Arrays
 # Plotting the Magnitude Spectrum
 # Display Graphical Overview
